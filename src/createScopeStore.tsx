8eec931783d99c8b3d474ea306b8581c492107bc
Keylenn <darklight_on@163.com>
2020-12-21 21:47:38 +0800

    feat: add default parameters & interrupt action update

--- src/createScopeStore.tsx
+++ src/createScopeStore.tsx
@@ -1,24 +1,30 @@
 import * as React from 'react'
 import {ActionCreatorsMapObject} from './types/action'
+import {TupleTail} from './types/common'
 
 const UNIQUE_SYMBOL = Symbol()
+const UNDEFINED = void 0
 
 export default function createScopeStore<I, A extends ActionCreatorsMapObject<I, A>>(
   initialState: I,
   actionCreatorsMap: A,
 ) {
-  type Actions = {[P in keyof A]: (...args: Parameters<A[P]>) => void}
+  type Actions = {[P in keyof A]: (...args: TupleTail<Parameters<A[P]>>) => void}
 
   const StateContext = React.createContext<I>(initialState)
   const DispatcherContext = React.createContext<Actions | typeof UNIQUE_SYMBOL>(UNIQUE_SYMBOL)
 
   function Provider({children}: {children: React.ReactNode}) {
     const [state, setState] = React.useState(initialState)
+    const stateRef = React.useRef(initialState)
+    stateRef.current = state
+
     const memoActions = React.useMemo(() => {
       const actions = {} as Actions
       for (const key in actionCreatorsMap) {
         actions[key] = async (...args: any[]) => {
-          const newState = await actionCreatorsMap[key](...args)
+          const newState = await actionCreatorsMap[key](stateRef.current, ...args)
+          if (newState === UNDEFINED) return
           setState(newState)
         }
       }
--- src/types/common.ts
+++ src/types/common.ts
@@ -9,3 +9,5 @@ export type ExcObj = Exclude<unknown, AnyObj>
 export interface ChiProvider {
   ({children}: {children: React.ReactNode}): JSX.Element
 }
+
+export type TupleTail<T extends any[]> = T extends [x: any, ...t: infer R] ? R : never
